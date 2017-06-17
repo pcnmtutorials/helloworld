caa8f681d425a44f9a9f0490ab1a314a15f1ef8a
jagadeesh <jagadeeshp7727@gmail.com>
2017-06-18 01:12:05 +0530

    sss

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('hello world');
+});
+
+app.listen(5244, function(){
+    console.log('server running at http://localhost:5244');
+});
